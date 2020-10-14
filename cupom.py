5602762ba059227afc08d18a4c7b123d5d07b133
whendev <whendel.morais.9@gmail.com>
2020-10-14 18:53:21 -0300

    Resolvido

--- cupom.py
+++ cupom.py
@@ -11,7 +11,34 @@ class Endereco:
     self.municipio = municipio
     self.estado = estado
     self.cep = cep
+  def validar_campos_obrigatorios(self):
+    if (self.logradouro == "" or self.logradouro == None):
+      raise Exception("O campo logradouro do endereço é obrigatório")
 
+    if (self.numero == 0):
+      self.numero = "s/n"
+    
+    if (self.municipio == "" or self.municipio == None):
+      raise Exception("O campo município do endereço é obrigatório")
+    
+    if (self.estado == "" or self.estado == None):
+      raise Exception("O campo estado do endereço é obrigatório")
+
+  def dados_endereco(self):
+      # Implemente aqui
+    self.validar_campos_obrigatorios()
+    
+    _COMPLEMENTO = " " + self.complemento if self.complemento else ""
+
+    _BAIRRO = self.bairro + " - " if self.bairro else ""
+
+    _CEP = "CEP:" + self.cep if self.cep else ""
+
+    _NUMERO =  self.numero if self.numero else "s/n"
+
+    return f'''{self.logradouro}, {_NUMERO}{_COMPLEMENTO}
+{_BAIRRO}{self.municipio} - {self.estado}
+{_CEP}'''
 
 class Loja:
   
@@ -23,11 +50,143 @@ class Loja:
     self.observacao = observacao
     self.cnpj = cnpj
     self.inscricao_estadual = inscricao_estadual
+    self.vendas = []
+
+  def vender(self, datahora, ccf, coo):
+    _venda = Venda(self, datahora, ccf, coo)
+    self.vendas.append(_venda)
+    return _venda
+  
+
+  def validar_campos_obrigatorios(self):
+    if (self.nome_loja == "" or self.nome_loja == None):
+      raise Exception("O campo nome da loja é obrigatório")
+    
+    if (self.cnpj == "" or self.cnpj == None):
+      raise Exception("O campo CNPJ da loja é obrigatório")
+    
+    if (self.inscricao_estadual == "" or self.inscricao_estadual == None):
+      raise Exception("O campo inscrição estadual da loja é obrigatório")
 
   def dados_loja(self):
     # Implemente aqui
-    return "" 
+    self.validar_campos_obrigatorios()
+
+    _TELEFONE = ""
+    
+    if(self.endereco.cep):
+      _TELEFONE = " Tel " + self.telefone if self.telefone else ""
+    else:
+      _TELEFONE = "Tel " + self.telefone if self.telefone else ""
+
+    _OBSERVACAO = self.observacao if self.observacao else ""
+
+    show = f'''{self.nome_loja}
+{self.endereco.dados_endereco()}{_TELEFONE}
+{_OBSERVACAO}
+CNPJ: {self.cnpj}
+IE: {self.inscricao_estadual}'''
+    return show
+
+class Produto:
+
+  def __init__(self, codigo, descricao, unidade, valor_unitario, substituicao_tributaria):
+    self.codigo = codigo
+    self.descricao = descricao
+    self.unidade = unidade
+    self.valor_unitario = valor_unitario
+    self.substituicao_tributaria = substituicao_tributaria
+
+class Item:
+
+  def __init__(self, venda, item, produto, quantidade):
+    self.venda = venda
+    self.item = item
+    self.produto = produto
+    self.quantidade = quantidade
+
+  def valor_item(self):
+    return self.quantidade * self.produto.valor_unitario
+
+  def dados_item(self):
+    return f'''{self.item} {self.produto.codigo} {self.produto.descricao} {self.quantidade} {self.produto.unidade} {self.produto.valor_unitario:.2f} {self.produto.substituicao_tributaria} {self.valor_item():.2f}\n'''
 
 class Venda:
-  pass
-  # Implemente aqui
+
+  def __init__(self, loja, datahora, ccf, coo):
+    self.loja = loja
+    self.datahora = datahora
+    self.ccf = ccf
+    self.coo = coo
+    self.itens = []
+    
+
+  def validar_campos_obrigatorios(self):
+    if not self.coo:
+      raise Exception("O campo coo da venda é obrigatório")
+    
+    if not self.ccf:
+      raise Exception("O campo ccf da venda é obrigatório")
+
+    if not self.datahora:
+      raise Exception("O campo data da venda é obrigatório")
+
+    if (self.itens.count == 0):
+      raise Exception("Você precisa inserir itens na sua venda")
+
+  def duplicado(self, produto, item):
+    for itemlist in self.itens:
+      if(itemlist.item != item and itemlist.produto.codigo == produto.codigo):
+        return True
+    return False
+
+  def valida_item(self, item, produto, quantidade):
+    if(self.duplicado(produto, item)):
+      raise Exception("Voce não pode inserir o mesmo produto com itens diferentes")
+
+    if(quantidade <= 0):
+      raise Exception("Insira a quantidade de itens")
+
+    if(produto.valor_unitario <= 0):
+      raise Exception("item não pode ser adicionado na venda com produto nesse estado")
+    
+
+  def adicionar_item(self, item, produto, quantidade):
+    self.valida_item(item, produto, quantidade)
+
+    _itemVenda = Item(self, item, produto, quantidade)
+    self.itens.append(_itemVenda)
+  
+  def dados_itens(self):
+    _dados = "ITEM CODIGO DESCRICAO QTD UN VL UNIT(R$) ST VL ITEM(R$)\n"
+
+    for item in self.itens:
+      _dados += item.dados_item()
+
+    return _dados
+
+  def calcular_total(self):
+    _total = 0
+    for i in self.itens:
+      _total += i.valor_item()
+    
+    return _total
+
+      
+  def dados_venda(self):
+    self.validar_campos_obrigatorios()
+
+    _texto_data = self.datahora.strftime("%d/%m/%Y")
+
+    _texto_hora = self.datahora.time().strftime("%H:%M:%S")
+
+    _dadosVenda = f'''{_texto_data} {_texto_hora}V CCF:{self.ccf} COO: {self.coo}'''
+    return _dadosVenda
+
+  def imprime_cupom(self):
+    _dadosLoja = self.loja.dados_loja()
+
+    _dadosVenda = self.dados_venda()
+    _dadosItens = self.dados_itens()
+    _cupom = "CUPOM FISCAL"
+    return f'''{_dadosLoja}\n------------------------------\n{_dadosVenda}\n{_cupom}\n{_dadosItens}------------------------------\nTOTAL R$ {self.calcular_total():.2f}'''
---
+++ venda_test.py
@@ -0,0 +1,176 @@
+# coding: utf-8
+
+import cupom
+import pytest
+from datetime import datetime
+
+def verifica_item(mensagem_esperada, Venda, itm, pdt, qnt):
+  with pytest.raises(Exception) as excinfo:
+    Venda.adicionar_item(itm, pdt, qnt)
+  the_exception = excinfo.value
+  assert mensagem_esperada == str(the_exception)
+
+
+# Todas as variaveis preenchidas
+
+DATA_HORA_VENDA = datetime(2020, 11, 25, 10, 30, 40)
+CCF = "021784"
+COO = "035804"
+
+
+NOME_LOJA = "Loja 1"
+LOGRADOURO = "Log 1"
+NUMERO = 10
+COMPLEMENTO = "C1"
+BAIRRO = "Bai 1"
+MUNICIPIO = "Mun 1"
+ESTADO = "E1"
+CEP = "11111-111"
+TELEFONE = "(11) 1111-1111"
+OBSERVACAO = "Obs 1"
+CNPJ = "11.111.111/1111-11"
+INSCRICAO_ESTADUAL = "123456789"
+
+# Dados gerais do teste
+
+produto1 = cupom.Produto(100, "Banana", "cx", 7.45, "ST")
+produto2 = cupom.Produto(101, "Laranja", "cx", 3.32, "ST")
+produto3 = cupom.Produto(102, "Leite", "l", 2.15, "")
+produto4 = cupom.Produto(103, "batata", "k", 0, "")
+
+
+ENDERECO_COMPLETO = cupom.Endereco(LOGRADOURO, NUMERO, COMPLEMENTO, BAIRRO,
+                                   MUNICIPIO, ESTADO, CEP)
+
+LOJA_COMPLETA = cupom.Loja(NOME_LOJA, ENDERECO_COMPLETO, TELEFONE, OBSERVACAO,
+                           CNPJ, INSCRICAO_ESTADUAL)
+
+VENDA_COMPLETA = LOJA_COMPLETA.vender(DATA_HORA_VENDA, CCF, COO)
+
+VENDA_COMPLETA.adicionar_item(1,produto1, 10)
+VENDA_COMPLETA.adicionar_item(2,produto2, 5)
+
+# Cenário 1
+
+TEXTO_ESPERADO_CENARIO_1 = '''Loja 1
+Log 1, 10 C1
+Bai 1 - Mun 1 - E1
+CEP:11111-111 Tel (11) 1111-1111
+Obs 1
+CNPJ: 11.111.111/1111-11
+IE: 123456789
+------------------------------
+25/11/2020 10:30:40V CCF:021784 COO: 035804
+CUPOM FISCAL
+ITEM CODIGO DESCRICAO QTD UN VL UNIT(R$) ST VL ITEM(R$)
+1 100 Banana 10 cx 7.45 ST 74.50
+2 101 Laranja 5 cx 3.32 ST 16.60
+------------------------------
+TOTAL R$ 91.10'''
+
+
+def test_cenario1_venda1():
+    assert (
+        VENDA_COMPLETA.imprime_cupom() == TEXTO_ESPERADO_CENARIO_1
+    )
+
+# Cenário 2
+
+TEXTO_ESPERADO_CENARIO_2_VENDA_1 = '''Loja 1
+Log 1, 10 C1
+Bai 1 - Mun 1 - E1
+CEP:11111-111 Tel (11) 1111-1111
+Obs 1
+CNPJ: 11.111.111/1111-11
+IE: 123456789
+------------------------------
+25/11/2020 10:30:40V CCF:021784 COO: 035804
+CUPOM FISCAL
+ITEM CODIGO DESCRICAO QTD UN VL UNIT(R$) ST VL ITEM(R$)
+1 100 Banana 1 cx 7.45 ST 7.45
+------------------------------
+TOTAL R$ 7.45'''
+
+cenario2_venda1 = LOJA_COMPLETA.vender(DATA_HORA_VENDA, CCF, COO)
+cenario2_venda1.adicionar_item(1, produto1, 1)
+
+def test_cenario2_venda1():
+    assert (
+        cenario2_venda1.imprime_cupom() == TEXTO_ESPERADO_CENARIO_2_VENDA_1
+    )
+
+TEXTO_ESPERADO_CENARIO_2_VENDA_2 = '''Loja 1
+Log 1, 10 C1
+Bai 1 - Mun 1 - E1
+CEP:11111-111 Tel (11) 1111-1111
+Obs 1
+CNPJ: 11.111.111/1111-11
+IE: 123456789
+------------------------------
+25/11/2020 10:30:40V CCF:021784 COO: 035804
+CUPOM FISCAL
+ITEM CODIGO DESCRICAO QTD UN VL UNIT(R$) ST VL ITEM(R$)
+1 101 Laranja 3 cx 3.32 ST 9.96
+------------------------------
+TOTAL R$ 9.96'''
+
+cenario2_venda2 = LOJA_COMPLETA.vender(DATA_HORA_VENDA, CCF, COO)
+cenario2_venda2.adicionar_item(1, produto2, 3)
+
+def test_cenario2_venda2():
+    assert (
+        cenario2_venda2.imprime_cupom() == TEXTO_ESPERADO_CENARIO_2_VENDA_2
+    )
+
+# Cenário 3
+
+TEXTO_ESPERADO_CENARIO_3 = '''Loja 1
+Log 1, 10 C1
+Bai 1 - Mun 1 - E1
+CEP:11111-111 Tel (11) 1111-1111
+Obs 1
+CNPJ: 11.111.111/1111-11
+IE: 123456789
+------------------------------
+25/11/2020 10:30:40V CCF:021784 COO: 035804
+CUPOM FISCAL
+ITEM CODIGO DESCRICAO QTD UN VL UNIT(R$) ST VL ITEM(R$)
+1 100 Banana 3 cx 7.45 ST 22.35
+2 102 Leite 5 l 2.15  10.75
+------------------------------
+TOTAL R$ 33.10'''
+
+
+cenario3_venda1 = LOJA_COMPLETA.vender(DATA_HORA_VENDA, CCF, COO)
+cenario3_venda1.adicionar_item(1, produto1, 3)
+cenario3_venda1.adicionar_item(2, produto3, 5)
+
+def test_cenario3_venda1():
+    assert (
+        cenario3_venda1.imprime_cupom() == TEXTO_ESPERADO_CENARIO_3
+    )
+
+#Cenario 4
+
+cenario4_venda1 = LOJA_COMPLETA.vender(DATA_HORA_VENDA, CCF, COO)
+cenario4_venda1.adicionar_item(1, produto1, 2)
+
+TEXTO_ESPERADO_CENARIO_4_VENDA_1 = "Voce não pode inserir o mesmo produto com itens diferentes"
+
+def test_cenario4_venda1():
+  verifica_item(TEXTO_ESPERADO_CENARIO_4_VENDA_1,cenario4_venda1, 2, produto1, 2)
+
+
+cenario4_venda2 = LOJA_COMPLETA.vender(DATA_HORA_VENDA, CCF, COO)
+
+TEXTO_ESPERADO_CENARIO_4_VENDA_2 = "Insira a quantidade de itens"
+
+def test_cenario4_venda2():
+  verifica_item(TEXTO_ESPERADO_CENARIO_4_VENDA_2, cenario4_venda2, 2, produto1, 0)
+
+cenario4_venda3 = LOJA_COMPLETA.vender(DATA_HORA_VENDA, CCF, COO)
+
+TEXTO_ESPERADO_CENARIO_4_VENDA_3 = "item não pode ser adicionado na venda com produto nesse estado"
+
+def test_cenario4_venda3():
+  verifica_item(TEXTO_ESPERADO_CENARIO_4_VENDA_3, cenario4_venda3, 2, produto4, 1)
